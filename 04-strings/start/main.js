ef9945d1b72e490edb526ec03e20754d9d42667c
primozw <primoz.weingerl@gmial.com>
2022-03-18 13:48:44 +0100

    04-strings

---
+++ 04-strings/start/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Strings (znakovni niz)
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ */
+const brand = 'BMW';
+const model = 'Mini';
+const serial = '2015-78345-160R'; // leto - število izdelanih avtov – oznaka motorja
+
+// Združite ime proizvajalca in modela v novo spremenljivko – uporabite operator + ali dobesedno predlogo (template literals)
+// Izpišite število znakov serijske številke
+// Izpišite zadnji znak serijske številke
+// Shranite leto proizvodnje v novo spremeljivko
+// Določite starost na avta
+// Shranite oznako motorja - zadnje 4 znake v novo spremenljivko
+// Izpišite število izdelanih avtov v istem letu
+// Izpišite ime proizvajalca in modela v minuskulah
+// Izpišite ime proizvajalca in modela v majuskulah
