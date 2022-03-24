64575cd8b7a6f866eebbe6c36ed3f7f04883adc8
primozw <primoz.weingerl@gmial.com>
2022-03-24 18:55:25 +0100

    variables and primitive data types (end)

---
+++ 03-variables-data-types/end/main.js
@@ -0,0 +1,36 @@
+'use strict';
+/**
+ * Primitivni (osnovni podatkovni tipi)
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ */
+
+// Definiraj proizvajalca avta kot niz znakov (string) 
+const brand = 'BMW';
+console.log(brand);
+
+let model = "Mini";
+console.log(model);
+
+// Definiraj barvo avta kot niz podatkov (string)
+let color = '#000';
+
+// Definiraj leto proizvodnje kot celo število (number)
+const year = 2015;
+
+// Definiraj prostornino motorja kot decimalno število (number)
+const engine = 1.8;
+
+console.log(typeof year );
+console.log(typeof engine );
+
+// Definiraj porabo kot decimalno število (number)
+const consumtion = 7.8;
+
+// Definiraj podporo sistema abs kot logično vrednost (boolean: true/false)
+let abs = true;
+
+// Definiraj lastnika kot prazno/ničelno vrednost
+let owner = null;
+
+
+
