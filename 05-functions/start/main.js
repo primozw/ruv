30ba4cbb408902e8d4cb48ca0490c250ed3bd96d
primozw <primoz.weingerl@gmial.com>
2022-03-24 21:46:15 +0100

    functions/start

---
+++ 05-functions/start/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Funkcije
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions 
+ */
+
+/*
+1. Zapišite funkcijo ki vrne pozdrav na podlagi podanega imena. Za obliko uporabite deklaracijo funkcije (function declaration).
+
+2. Preoblikujte zapis, tako da bo funkcija definirana kot izraz (function expression).
+
+3. Zapišite funkcijo za izračun stroškov prevoza. Vhodni parametri: razdalja, poraba (na 100 km), cena goriva (na liter).
+
+4. Definirajte privzete vrednosti vhodnih parametrov: poraba in cena goriva
+
+5. Ustvarite funkcijo, ki bo služila za štetje avtomobilov (counter). Funkcija mora imeti naslednje lastnosti:
+– vsakič ko pokličemo funkcijo se bo števec povečal za ena in vrnil skupno število avtomobilov
+– števca ne moremo spremeniti – skupno število avtov je odvisno samo od tega kolikokrat smo poklicali funkcijo
+*/
+
+
+
