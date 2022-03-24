c71256280de3206fcb227fd41dbcbbb90f8c75b4
primozw <primoz.weingerl@gmial.com>
2022-03-24 21:47:12 +0100

    conditional-loops/start

---
+++ 06-conditional-loops/start/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const model = "mini";
+const consumption  = 6.5;
+
+// 1. Napišite funckijo, ki na podlagi podane porabe avtomobila vrnila ekološki razrad:
+// – če je poraba manjša ali enaka 4 je razred 1
+// – če je poraba manjša ali enaka 7 je razred 2
+// – če je poraba manjša ali enaka 12 je razred 3
+// – če je porava večja od 12 je razred 4
+
+
+// 2. Napišite funkcijo, ki na podlagi modela vrne znamko modela:
+// mini --> BMW
+// fabia --> Skoda
+// karoq --> Skoda
+// v60 --> Volvo
+// ostalo --> Unknown
+
+
+// 3. Napišite funkcijo, ki izpiše vsa cela števila od 1 do podanega parametra:
+// disp(5) --> 1 2 3 4 5
+// a) uporabite for zanko
+// b) uporabite while zanko
+
