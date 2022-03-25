90a7072832bfd524982c117b3f7c94f5d80dd435
primozw <primoz.weingerl@gmial.com>
2022-03-25 15:33:33 +0100

    strings/end

--- 04-strings/end/main.js
+++ 04-strings/end/main.js
@@ -6,7 +6,7 @@
  */
 const brand = 'BMW';
 const model = 'Mini';
-const serial = '2015-78345-160R'; // leto - število izdelanih avtov – oznaka motorja
+const serial = '2015-5678765444-160R'; // leto - število izdelanih avtov – oznaka motorja
 
 // Združite ime proizvajalca in modela v novo spremenljivko – uporabite operator + ali dobesedno predlogo (template literals)
 let dispInfo = brand + ' ' + model;
@@ -15,10 +15,28 @@ let dispBrandNew = 'Ta avto je \n super kul'
 console.log(dispBrandNew);
 
 // Izpišite število znakov serijske številke
+console.log(dispNew.length);
+
 // Izpišite zadnji znak serijske številke
+console.log(dispNew[dispNew.length - 1]);
+
 // Shranite leto proizvodnje v novo spremeljivko
+let year = serial.slice(0,4)
+console.log(year);
+
 // Določite starost na avta
+const age = new Date().getFullYear() - year;
+console.log(age);
+
 // Shranite oznako motorja - zadnje 4 znake v novo spremenljivko
+const engine = serial.slice(-4);
+
 // Izpišite število izdelanih avtov v istem letu
+let seqNumber = serial.slice(serial.indexOf('-') + 1, serial.lastIndexOf('-'));
+console.log('Število izdelanih avtomobilov:', seqNumber);
+
 // Izpišite ime proizvajalca in modela v minuskulah
+console.log(brand.toLocaleLowerCase());
+
 // Izpišite ime proizvajalca in modela v majuskulah
+console.log(brand.toUpperCase());
