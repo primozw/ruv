05cd0fdd4271e1067d0caf0d1973b9b3425db5fa
primozw <primoz.weingerl@gmial.com>
2022-03-31 19:05:45 +0200

    arrays/start

---
+++ 07-arrays/start/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Arrays (polja)
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+const serial = '2015-7834-160R';
+
+// Definirajte polje barv, polje naj vključuje naslednje barve: Red, DarkTurquoise, Teal
+
+// Iz serijske številke ustvarite polje posameznih vrednosti
+
+// Shranite letnico izdelave v samostojno spremneljivko
+
+// Preverite ali je Teal na seznamu barv
+
+// Dodajte barvo White na konec seznama
+
+// Dodajte barvo Crimson na začetek seznama
+
+// Odstranite zadnjo barvo na seznamu
+
+// Zamenjajte barvo Red z barvo DarkCyan
+
+// Omenjene barve definirajte v barvnem prostoru HSL in vrednosti zapište znotraj polja imenovanega hslColors
+
+// Za vsako barvo izpišite njeno ime in pripadajočo svetlost barve
+
+// Vrednost barve zapišite v novo polje, ki bo namesto znakovnih nizov vsebovala polja posameznih vrednosti hsl(348, 83%, 58%) --> [348, 83%, 58%]
+
+// Izbrišite barve, ki imajo svetlost večjo od 50%
