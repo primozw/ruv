cf164f3839502374533f63c81665669b8f4aa655
primozw <primoz.weingerl@gmial.com>
2022-03-31 19:07:25 +0200

    objects/start

---
+++ 08-objects/start/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Objects (objekti)
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+ */
+
+// Ustvarite nov objekt, ki bo bo vseboval lastnosti določenega avta (proizvajalec, model, letnica izdelava, barva ...)
+// Izpišite poljubno lastnost avta
+// Spremenite poljubno lastnost avta
+// Objektu dodajte funkcijo getDesc (metodo), ki vrnila znakovni niz sestavljen iz  proizvajalca in modela avta 
+// Objektu dodajte funkcijo setSerial (metodo), za ustvarjanje serijske številke, ki je sestavljena iz leta in naključnega števila: npr. 2015-32435435
+// – vhodni parameter funcije naj bo dolžina naključnega števila - število števk
+// – funkcija pa naj doda serijsko številko objektu kot njegovo lastnost 
+// Ustvarite graditeljsko funkcijo (constuctor function) imenovano Car za ustvarjanje objekta, ki ima iste lastnosti in metode kot prej ustvarjen objekt 
+// Funkcije dodajte prototipu graditeljske funkcije
+// Z uporabo graditeljske funkcije ustvarite nekaj novih objektov
+// Izpišite vse lastnosti ustvarjenih objektov
+// Ustvarite kopijo objekta in mu dodajte novo lastnost
