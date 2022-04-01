782240222b8b824a4110c0d0dd202daf4de62696
primozw <primoz.weingerl@gmial.com>
2022-04-01 09:43:50 +0200

    arrays/start

--- 07-arrays/start/main.js
+++ 07-arrays/start/main.js
@@ -7,13 +7,11 @@
 
 const serial = '2015-7834-160R';
 
-// Definirajte polje barv, polje naj vključuje naslednje barve: Red, DarkTurquoise, Teal
+// Definirajte polje barv, polje naj vključuje naslednje barve: Red, DarkTurquoise, Teal, DarkSalmon, DeepPink
 
 // Iz serijske številke ustvarite polje posameznih vrednosti
 
 // Shranite letnico izdelave v samostojno spremneljivko
-
-// Preverite ali je Teal na seznamu barv
 
 // Dodajte barvo White na konec seznama
 
@@ -21,7 +19,17 @@ const serial = '2015-7834-160R';
 
 // Odstranite zadnjo barvo na seznamu
 
+// Nadomestite barvo, ki se nahaja na sredini seznama s poljubno barvo
+
+// Združite vse barve v znakovni niz. Barve naj bodo ločene z vejico
+
 // Zamenjajte barvo Red z barvo DarkCyan
+
+// Preverite ali je Teal na seznamu barv
+
+// Razvrstite barve po abecednem vrstnem redu
+
+// Razvrstite barve v nasprotnem abecednem redu
 
 // Omenjene barve definirajte v barvnem prostoru HSL in vrednosti zapište znotraj polja imenovanega hslColors
 
@@ -30,3 +38,6 @@ const serial = '2015-7834-160R';
 // Vrednost barve zapišite v novo polje, ki bo namesto znakovnih nizov vsebovala polja posameznih vrednosti hsl(348, 83%, 58%) --> [348, 83%, 58%]
 
 // Izbrišite barve, ki imajo svetlost večjo od 50%
+
+// Dodeljevanje reference in kopiranje
+// Kopirajte polja barv v novo neodvisno spremenljivko 
